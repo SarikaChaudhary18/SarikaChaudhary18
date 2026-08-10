aa13e6cf97d16643e3d2ec442daa32a36835b054
SarikaChaudhary18 <sarikachaudhary231@gmail.com>
2026-08-10 16:50:43 +0530

    Time: 0 ms (100%), Space: 12.6 MB (12.03%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> ans;
-        std::merge(nums1.begin(),nums1.begin()+m,
-        nums2.begin(),nums2.begin()+n,
-        back_inserter(ans));
-       nums1=ans;
-        for(int x:ans){
-            cout<<x<<" ";
+       int left=m-1;
+       int right=0;
+       while(left>=0 && right<n){
+        if(nums1[left]>nums2[right]){
+            swap(nums1[left],nums2[right]);
+            left--;
+            right++;
+        }else{
+            break;
         }
+       }
+       sort(nums1.begin(),nums1.begin()+m);
+       sort(nums2.begin(),nums2.begin()+n);
+       for(int i=0;i<n;i++){
+        nums1[m+i]=nums2[i];
+       }
     }
 };
