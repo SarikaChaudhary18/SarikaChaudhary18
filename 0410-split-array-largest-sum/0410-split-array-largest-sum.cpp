48e6c9dfdb40330a11fb37e64e430ef6ffb7077b
SarikaChaudhary18 <sarikachaudhary231@gmail.com>
2026-08-29 12:59:16 +0530

    Time: 0 ms (100%), Space: 10.5 MB (43.39%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-bool canSplit(vector<int>& nums, int k, long long maxSum) {
-        int parts = 1;
-        long long currentSum = 0;
-        for (int x : nums) {
-            if (currentSum + x <= maxSum) {
-                currentSum += x;
-            }
-            else {
-                parts++;
-                currentSum = x;
+    bool canSplit(vector<int> &nums,int k,long long maxSum){
+        int count=1;
+        long long sum=0;
+        for(int i=0;i<nums.size();i++){
+            if(sum+nums[i]<=maxSum){
+                sum+=nums[i];
+            }else{
+                count++;
+                sum=nums[i];
             }
         }
-        return parts <= k;
+        return count<=k;
     }
     int splitArray(vector<int>& nums, int k) {
-        long long low = 0;
-        long long high = 0;
-        for (int x : nums) {
-            low = max(low, (long long)x);
-            high += x;
+       long long low=*max_element(nums.begin(),nums.end());
+       long long high=accumulate(nums.begin(),nums.end(),0LL); 
+       long long ans=high;
+       while(low<=high){
+        long long mid=(low+high)/2;
+        if(canSplit(nums,k,mid)){
+            ans=mid;
+            high=mid-1;
+        }else{
+            low=mid+1;
         }
-        while (low <= high) {
-            long long mid = low + (high - low) / 2;
-            if (canSplit(nums, k, mid)) {        
-                high = mid - 1;
-            }
-            else {             
-                low = mid + 1;
-            }
-        }
-        return (int)low;
+       }
+       return ans;
     }
 };
