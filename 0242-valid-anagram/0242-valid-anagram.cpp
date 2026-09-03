7c9c13bc7336942f8dbcbbdaf4b0315bf5561a52
SarikaChaudhary18 <sarikachaudhary231@gmail.com>
2026-09-03 22:45:54 +0530

    Time: 0 ms (100%), Space: 9.8 MB (25.88%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> mpp;
+        for(char c:s){
+            mpp[c]++;
+        }
+        for(char c:t){
+            mpp[c]--;
+        }
+        for(auto x:mpp){
+            if(x.second!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
